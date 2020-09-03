2eba367be7d89716b25af58b94dd37e7b1d9b9fb
Daniel Calvanese <Daniel.Calvanese@Gmail.com>
2020-09-02 21:53:49 -0400

    Apparently green has to be lowercase.

--- stats/attendance.py
+++ stats/attendance.py
@@ -17,7 +17,7 @@ attendance.plot(x='year', y='attendance', figsize=(15,7), kind='bar');
 # Add plot metadata.
 plt.xlabel('Year');
 plt.ylabel('Attendance');
-plt.axhline(y=attendance['attendance'].mean(), label='Mean', linestyle='--', color='Green');
+plt.axhline(y=attendance['attendance'].mean(), label='Mean', linestyle='--', color='green');
 
 # Show the plot.
 plt.show();
